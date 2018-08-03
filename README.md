151c1f56ce3a969e9f8db4566cc3a0233b4db927
mninc <manicminer106@gmail.com>
2018-08-03 15:56:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# premium history links
+
+Get all the history links from a premium search page so you can give them to another user to suggest.
+
+## Installation
+
+1. Install [Greasemonkey (Firefox)](http://www.greasespot.net/) or [Tampermonkey (Chrome)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+2. Open the [script in raw view](https://github.com/mninc/premium-history-links/raw/master/premium-history-links.js).
+3. Install.
+
+
+## In use
+![Example](https://media.discordapp.net/attachments/337943916613599234/474952864582598657/prem.PNG)
---
+++ premium-history-links.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         premium history links
+// @namespace    https://steamcommunity.com/id/manic_/
+// @version      1.0
+// @description  all the history links to copy and paste
+// @author       manic
+// @grant        none
+// @license      MIT
+
+// @homepageURL     https://github.com/mninc/premium-history-links
+// @supportURL      https://github.com/mninc/premium-history-links/issues
+// @downloadURL     https://github.com/mninc/premium-history-links/raw/master/premium-history-links.js
+
+// @run-at       document-end
+// @match        https://backpack.tf/premium/search*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let div = document.createElement("div");
+    div.className = "premium-search-form";
+    let header = document.createElement("h2");
+    header.className = "title";
+    header.innerHTML = "Links";
+    div.appendChild(header);
+
+    let text = [];
+    let links = document.getElementsByClassName("btn btn-default btn-xs");
+    let prem = false;
+    for (let i = 0; i < links.length; i++) {
+        let a = links[i];
+        if (a.href.includes("/item/")) {
+            text.push(a.href + " ");
+            prem = true;
+        }
+
+    }
+
+    let p, small;
+    if (prem) {
+        p = document.createElement("p");
+        p.className = "warning";
+
+        small = document.createElement("small");
+        small.className = "text-muted";
+        small.innerHTML = "All history links on this page. Clicking the text box will automatically copy the links to clipboard.";
+        p.appendChild(small);
+        div.appendChild(p);
+
+        let box = document.createElement("textarea");
+        box.readonly;
+        box.spellcheck = false;
+        box.onclick = function() {this.focus();this.select();document.execCommand('copy');};
+        box.cols = 35;
+        box.rows = text.length;
+        box.value = text.join("\n");
+        div.appendChild(box);
+    } else {
+        p = document.createElement("p");
+        p.className = "warning";
+
+        small = document.createElement("small");
+        small.className = "text-muted";
+        small.innerHTML = "You do not have premium!";
+
+        p.appendChild(small);
+        div.appendChild(p);
+    }
+
+
+
+    let col = document.getElementsByClassName("col-md-4")[0];
+    col.appendChild(div);
+})();
