ccd2cc0f4c0dcfe5142c248249a50501a081cf56
mninc <manicminer106@gmail.com>
2018-08-24 13:06:25 +0100

    toggle

--- premium-history-links.user.js
+++ premium-history-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         premium history links
 // @namespace    https://steamcommunity.com/id/manic_/
-// @version      1.1
+// @version      1.2
 // @description  all the history links to copy and paste
 // @author       manic
 // @grant        none
@@ -52,7 +52,8 @@
                     // not all last-exchanged buttons loaded yet
                     setTimeout(wait, 100);
                 } else {
-                    let links = [];
+                    let links2 = [];
+                    let links3 = [];
 
                     let button_groups = document.getElementsByClassName("buttons btn-group");
                     for (let i = 0; i < button_groups.length; i++) {
@@ -73,31 +74,71 @@
 
                         let days_old = parseInt(last_exchange.innerText.split(" ")[0]);
                         if (days_old > 90) continue;
-
-
-                        links.push("https://backpack.tf/item/" + last_exchange.getAttribute("data-original-id").toString());
+                        links3.push("https://backpack.tf/item/" + last_exchange.getAttribute("data-original-id").toString());
+                        if (days_old > 60) continue;
+                        links2.push("https://backpack.tf/item/" + last_exchange.getAttribute("data-original-id").toString());
 
 
                     }
 
-                    let p, small;
-
-                    p = document.createElement("p");
+                    let p = document.createElement("p");
                     p.className = "warning";
 
-                    small = document.createElement("small");
+                    let small = document.createElement("small");
                     small.className = "text-muted";
                     small.innerHTML = "All in-date history links on this page. Clicking the text box will automatically copy the links to clipboard.";
                     p.appendChild(small);
                     div.appendChild(p);
+
+                    let toggle = document.createElement("div");
+                    toggle.className = "buttons btn-group";
+
+                    for (let i = 0; i < 2; i++) {
+                        let option = document.createElement("a");
+                        if (i === 0) {
+                            option.innerText = "2 months";
+                            option.id = "two_months";
+
+                        } else {
+                            option.innerText = "3 months";
+                            option.id = "three_months";
+                            option.style["background-color"] = "#e6e6e6";
+                            option.style["border-color"] = "#adadad";
+                        }
+                        option.className = "btn btn-default btn-xs";
+                        option.onclick = function() {
+                            let other;
+                            let box = document.getElementById("box");
+                            if (this.id === "three_months") {
+                                other = document.getElementById("two_months");
+                                box.rows = links3.length;
+                                box.value = links3.join("\n");
+                            } else {
+                                other = document.getElementById("three_months");
+                                box.rows = links2.length;
+                                box.value = links2.join("\n");
+                            }
+
+                            this.style["background-color"] = "#e6e6e6";
+                            this.style["border-color"] = "#adadad";
+                            other.style["background-color"] = "#fff";
+                            other.style["border-color"] = "#ccc";
+
+
+                        };
+                        toggle.appendChild(option);
+                    }
+                    div.appendChild(toggle);
+
 
                     let box = document.createElement("textarea");
                     box.readonly;
                     box.spellcheck = false;
                     box.onclick = function() {this.focus();this.select();document.execCommand('copy');};
                     box.cols = 35;
-                    box.rows = links.length;
-                    box.value = links.join("\n");
+                    box.rows = links3.length;
+                    box.value = links3.join("\n");
+                    box.id = "box";
                     div.appendChild(box);
 
                     col.appendChild(div);
